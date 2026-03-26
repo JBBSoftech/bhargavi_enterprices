f868785763bcbdd29e8512f489b80a307b7de865
JBBSoftech <jbbsoftech@gmail.com>
2026-03-26 18:42:46 +0530

    Update Appifyours/frontend/lib/screens/element_screen/delivery.dart

--- Appifyours/frontend/lib/screens/element_screen/delivery.dart
+++ Appifyours/frontend/lib/screens/element_screen/delivery.dart
@@ -18,7 +18,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
-import 'package:appifyours/services/api_service.dart';
+import 'package:frontend/services/api_service.dart';
+
 // ================================================================
 // ⚙️  CONFIG — ONLY EDIT THIS SECTION
 // ================================================================
